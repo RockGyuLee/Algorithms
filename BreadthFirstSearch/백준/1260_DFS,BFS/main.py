a4a8740b25a19d7932be14f786935396b82e385b
RockGyuLee <rmfoszld@naver.com>
2021-12-18 14:44:26 +0900

    BreadthFirstSearch 백준 알고리즘 미로 풀이

--- BreadthFirstSearch/백준/1260_DFS,BFS/main.py
+++ BreadthFirstSearch/백준/1260_DFS,BFS/main.py
@@ -4,7 +4,7 @@ def bfs(gr, start, visited) :
     queue = deque([start])
 
     visited[start] = True
-    print(gr, start,visited)
+
     while queue :
         v = queue.popleft()
         print(v, end=" ")
@@ -12,6 +12,16 @@ def bfs(gr, start, visited) :
             if not visited[i] :
                 queue.append(i)
                 visited[i] = True
+    print("")
+
+def dfs(gr, start, visited) :
+
+    visited[start] = True
+    print(start, end=" ")
+
+    for i in gr[start]:
+        if not visited[i] :
+            dfs(gr,i,visited)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -26,9 +36,14 @@ if __name__ == '__main__':
 
         graph[i].append(j)
         graph[j].append(i)
-
     for i in range(len(graph)) :
         graph[i].sort()
+
+    dfs(graph, v, visited)
+    print()
+    visited[0] = True
+
+    visited = [False] * (n + 1)
 
     bfs(graph, v, visited)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ BreadthFirstSearch/백준/2178_미로/main.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+def findRoadMiro(gr, start) :
+
+    moveX = [0,1,0,-1]
+    moveY = [1,0,-1,0]
+
+    queue = deque([start])
+    while queue :
+        v = queue.popleft()
+        for i in range(4) :
+            x = v[0] + moveX[i]
+            y = v[1] + moveY[i]
+            if isCheck(x,y):
+                if gr[x][y] == 1 :
+                    gr[x][y] += gr[v[0]][v[1]]
+                    queue.append([x,y])
+
+def isCheck(x,y) :
+    if 0 <= x < n and 0 <= y < m :
+        return True
+    return False
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    n,m = map(int,input().split(" "))
+    graph = []
+
+    for _ in range(n):
+        ar = list(map(int,input()))
+        graph.append(ar)
+
+    findRoadMiro(graph,[0,0])
+    print(graph[n-1][m-1])
+"""
+3 3
+100
+111
+101
+"""
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
